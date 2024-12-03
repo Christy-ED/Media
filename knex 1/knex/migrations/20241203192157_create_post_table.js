6290056af33c21a914d1afeb35c1676a054b53fa
Christy <Edgardchristianne@gmail.com>
2024-12-03 15:28:31 -0500

    created the post table

--- knex 1/knex/migrations/20241203192157_create_post_table.js
+++ knex 1/knex/migrations/20241203192157_create_post_table.js
@@ -1,18 +1,29 @@
 exports.up= async function(knex){
     try{
-        await knex.schema.createTable("users", (table) => {
-            table.increment("id").primary();
+        await knex.schema.createTable("posts", (table) => {
+            table.increments("id").primary();
             table.string("content").notNullable().unique();
             table
-            .integer("user_id")// Foreign key column
-            .unsigned()
-            .notNullable()
-            .references("id")// Reference the id colum in the users table
-            .inTable("users")
-            .onDelete("CASCADE")// Delete post if the uer is deleted
-            .onUpdate("CASCADE")// Update foreign key  on user 'id' changes
-          
-        })
-  
+              .integer("user_id")
+              .unsigned()
+              .notNullable()
+              //The posts table references the users table through the user_id column.
+              .references("id").inTable("users")
+              .onDelete("CASCADE") // Delete post if the uer is deleted
+              .onUpdate("CASCADE");// Update foreign key  on user 'id' changes
+        });
+        console.log("Table 'posts' created successfully ");
+    } catch(error) {
+        console.log("Error creating able 'posts' :", error);
+        throw error;
+    }
+};
+
+exports.down = async function(knex) {
+    try{
+        await knex.cshema.dropTableIfExits("posts");
+        console.log("Table 'posts' dropped succesfully");
+    } catch (error) {
+        console.error("Error dropping table 'posts':", error);
     }
 };
