4cbe9825d0a10a1cd6373fc7e08eba68eb3db667
Christy <Edgardchristianne@gmail.com>
2025-01-07 14:11:50 -0500

    adding a frontend to this project

---
+++ frontend/src/pages/Loging.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useState from 'react'
+
+const Loging = () => {
+
+    const [loging, setLogin] = useState;
+    
+  return (
+    <div>Loging</div>
+  )
+}
+
+export default Loging
